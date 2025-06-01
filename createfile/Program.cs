9d7a6a66de4f08891aeb1ec7cf7c924af55d0cdb
Maryam Mohammadi <maryammohammadii684@gmail.com>
2025-06-01 16:33:33 +0330

    Add project files.

---
+++ createfile/Program.cs
@@ -0,0 +1,26 @@
+﻿using OfficeOpenXml;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        var file = new FileInfo(@"C:\Users\T630\Desktop\Maryam\Book1.xlsx");
+
+        using (var package = new ExcelPackage(file))
+        {
+            var ws = package.Workbook.Worksheets[1];
+
+            for (int r = 1; r <= ws.Dimension.End.Row; r++)
+            {
+                for (int c = 1; c <= ws.Dimension.End.Column; c++)
+                {
+                    Console.Write(ws.Cells[r, c].Text + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
